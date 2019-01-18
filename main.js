509d98e55edd030faf741dde12611803ce376dac
jackajackalop <yixinh@andrew.cmu.edu>
2019-01-17 21:30:17 -0500

    better chomp detection and video hidden

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function setup() {
     var videoInput = createCapture();
     videoInput.size(400, 300);
     videoInput.position(0, 0);
-    
+    videoInput.hide();
     // setup canvas
     var cnv = createCanvas(400, 300);
     cnv.position(0, 0);
@@ -41,14 +41,16 @@ function checkMouth(A, B){
 	if(prevDist-currentDist<0){
 		prevDist = currentDist;
 		open = true;
-		console.log("OPEN!");
+//		console.log("OPEN!");
 	}
-
 	if(open && prevDist-currentDist>=2.0){
+        open = false;
+        prevDist = currentDist;
 		console.log("CHOMP!");
-		open = false;
-		prevDist = currentDist;
-	} 
+	}
+    if(!open && prevDist-currentDist>0){
+        prevDist = currentDist;
+    }
 
 }
 
@@ -62,6 +64,6 @@ function draw() {
       	// set the color of the ellipse based on position on screen
       	fill(map(positions[i][0], width*0.33, width*0.66, 0, 255), map(positions[i][1], height*0.33, height*0.66, 0, 255), 255);
       	// draw ellipse at each position point
-      	ellipse(positions[i][0], positions[i][1], 8, 8);
+      	//ellipse(positions[i][0], positions[i][1], 8, 8);
     }
 }
