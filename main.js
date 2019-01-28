ea4e4095694ba542cc56320f4c13f10cd5c52ce7
jackajackalop <yixinh@andrew.cmu.edu>
2019-01-27 22:34:22 -0500

    currently everything planned has been implemented

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ var wide_eyes = false;
 var prevDist_mouth;
 var eyeDist;
 var headDist;
+var noseDist;
 var tilt = false;
 var ears;
 var eyes;
 var muzzle;
+var full;
+var face;
+var search = true;
 
 function setup() {
     // setup camera capture
@@ -30,6 +34,21 @@ function setup() {
     muzzle = loadModel('\\muzzle.obj');
     eyes = loadModel('\\eyes.obj');
     ears = loadModel('\\ears.obj');
+    face = loadModel('\\face.obj');
+}
+
+function keyPressed(){
+    if(keyCode == 32){
+        open_mouth = false;
+        chomp = false;
+        wide_eyes = false;
+        prevDist_mouth = undefined;
+        eyeDist = undefined;
+        headDist = undefined;
+        noseDist = undefined;
+        tilt = false;
+        search = true;
+    }
 }
 
 function distance(A, B) {
@@ -51,8 +70,8 @@ function checkMouth(A, B){
 	}
 	if(open_mouth && prevDist-currentDist>=5.0){
         chomp = true;
+        search = false;
         console.log("CHOMP!");
-
 	}
     if(!open_mouth && prevDist-currentDist>0){
         prevDist = currentDist;
@@ -66,18 +85,37 @@ function checkEyes(eyeR1, eyeR2, eyeL1, eyeL2){
     // var distL = distance(eyeL1, eyeL2);
     if(eyeDist == undefined) eyeDist = distR;
     else{
-        if(distR-eyeDist>2) wide_eyes = true;
-        if(wide_eyes)console.log("STARE!");        
+        if(distR-eyeDist>2){
+            wide_eyes = true;
+            search = false;
+            console.log("STARE!");   
+        }     
     }
 }
 
-function checkTilt(chin){
-    var dist = distance([0,0], chin);
+function checkTilt(head){
+    var dist = distance([0,0], head);
     if(headDist == undefined) headDist = dist;
     else{
-        if(abs(headDist-dist)>10) tilt = true;
-        // console.log(dist);
-        if(tilt)console.log("TILT!");        
+        if(abs(headDist-dist)>10){
+            tilt = true;
+            search = false;
+            console.log("TILT!"); 
+        }
+        // console.log(dist);       
+    }
+}
+
+function checkFace(nose){
+    var dist = distance([0,0], nose);
+    if(noseDist == undefined) noseDist = dist;
+    else{
+        if(abs(noseDist-dist)>10){
+            full = true;
+            search = false;
+            console.log("FACE!"); 
+        }
+        // console.log(dist);       
     }
 }
 
@@ -86,12 +124,12 @@ function draw() {
     // get array of face marker positions [x, y] format
     var positions = ctracker.getCurrentPosition();
     if(positions.length>=57){
-        if(!chomp)
+        if(search){
             checkMouth(positions[57], positions[32]);
-        if(!wide_eyes) 
             checkEyes(positions[24], positions[26], positions[29], positions[31]);
-        if(!tilt) 
-            checkTilt(positions[7]);
+            checkTilt(positions[0]);
+            checkFace(positions[62]);
+        }
     // for (var i=0; i<positions.length; i++) {
     //   	fill(map(positions[i][0], width*0.33, width*0.66, 0, 255), map(positions[i][1], height*0.33, height*0.66, 0, 255), 255);
     //   	ellipse(positions[i][0], positions[i][1], 8, 8);
@@ -101,9 +139,10 @@ function draw() {
         ambientMaterial(200);
         ambientLight(200);
         pointLight(250, 250, 250, -200, -200, 200);
-        // if(chomp) drawChomp(positions[57]);
-        // if(wide_eyes) drawEyes(positions[24], positions[29]);
-        if(tilt) drawEars(positions[0], positions[14]);
+        if(chomp) drawChomp(positions[57]);
+        else if(wide_eyes) drawEyes(positions[24], positions[29]);
+        else if(tilt) drawEars(positions[0], positions[14]);
+        else if(full) drawFace(positions[62]);
     }
 }
 
@@ -120,7 +159,7 @@ function drawEyes(r, l){
     scale(25);
     rotateX(180);
     rotateY(180);
-    translate(-(l[0]-445)/10, -(l[1]-220)/10, -20);
+    translate(-(l[0]-455)/10, -(l[1]-230)/10, -20);
     model(eyes);
     resetMatrix();
 }
@@ -133,5 +172,13 @@ function drawEars(r, l){
     translate(-(pt[0]-400)/10, -(pt[1]-280)/10, -20);
     model(ears);
     resetMatrix();
+}
 
+function drawFace(nose){
+    scale(40);
+    rotateX(180);
+    rotateY(180);
+    translate(-(nose[0]-400)/10, -(nose[1]-280)/10, -30);
+    model(face);
+    resetMatrix();
 }
